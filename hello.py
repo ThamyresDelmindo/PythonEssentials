baf83215bfedf4ebeb3a13e563854dedc65560b0
Thamyres Delmindo <thamyres.delmindo22@gmail.com>
2025-05-14 23:32:35 -0300

    Quase perdi essa att. Inseri informações de idioma

--- hello.py
+++ hello.py
@@ -1,3 +1,37 @@
 #!/usr/bin/env python3
-print("Hello, world!")
-#este programa imprime Hello World
+"""Documentação do meu programa.
+Hello World - Português - BR
+
+Meu primeiro script em Python realizado pelo treinamento Phyton Base da 
+LinuxTips.
+
+Dependendo da língua configurada no ambiente o 
+programa exibe a mensagem correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+    export LANG=pt_BR
+Execução:
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Thamyres Delmindo"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+    
+print(msg)
+    
