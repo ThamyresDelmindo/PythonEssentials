c20a9135c85154e0fd2045eac6d1c08a399c2b49
Thamyres Delmindo <thamyres.delmindo22@gmail.com>
2025-05-20 17:55:49 -0300

    Criação de um texto básico de email

---
+++ interpolacao.py
@@ -0,0 +1,17 @@
+Olá, %(nome)s
+    ...: 
+    ...: Tem interesse em comprar uma  %(produto)s?
+    ...: 
+    ...: Este produto é ótimo para resolver 
+    ...: %(texto)s
+    ...: 
+    ...: Clique agora em %(link)s
+    ...: 
+    ...: Apenas %(quantidade)d disponíveis!
+    ...: 
+    ...: Preço promocional %(preço).2f
+    ...: """
+
+In [71]: for cliente in clientes:
+    ...:     print(email_tmpl % {"nome": cliente, "produto": "caneta", "texto": "Problemas de má escrita 
+       ⋮ e dores no pulso", "link": "https://canetaslegais.com", "quantidade": 1, "preço": 50.5, }) 
