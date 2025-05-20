9b564d1dd00d28b8cb00c1108ff6c620eb87c106
Thamyres Delmindo <thamyres.delmindo22@gmail.com>
2025-05-20 17:54:32 -0300

    Coloquei a tabuada uma ao lado da outra. Uhull

--- tabuada.py
+++ tabuada.py
@@ -2,3 +2,22 @@
 """Imprime a tabuada do 1 ao 10"""
 __version__ = "0.1.0"
 ___author___ = "Thamyres"
+
+# iterable(percorríveis, pode percorrer cada um dos itens, um a um)
+# base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+numeros = list (range(1,11))
+largura = 18
+
+for n in numeros:
+    print("{:^{}}".format(f"Tabuada do {n}", largura), end="")
+print()
+
+print("-" * largura * len(numeros))
+
+for linha in range (1, 11):
+    for n in numeros:
+        print("{:^{}}".format(f"{n} x {linha} = {n*linha}", largura), end="")  
+    print()     
+
+print("-" * largura * len(numeros))
