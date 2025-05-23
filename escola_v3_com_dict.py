1b19d447a91bc4b33b4afaa4a1420d8b539011c7
Thamyres Delmindo <thamyres.delmindo22@gmail.com>
2025-05-23 17:30:28 -0300

    Criei sozinha, só que não, com ajuda, o dicionário. Deu certo

--- escola_v3_com_dict.py
+++ escola_v3_com_dict.py
@@ -7,34 +7,52 @@ que frequentam cada uma das atividades.
 
 __version__ = "0.1.2"
 
-# Dados
-sala1 = ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
-sala2 = ["João", "Antonio", "Carlos", "Maria", "Isolda"]
+# DICIONARIO
 
-aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
-aula_musica = ["Erik", "Carlos", "Maria"]
-aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
+aulas = {
+    "aula_ingles": ["Erik", "Maia", "Joana", "Carlos", "Antonio"], 
+    "aula_musica": ["Erik", "Carlos", "Maria"],
+    "aula_danca": ["Gustavo", "Sofia", "Joana", "Antonio"],
+}
+salas = {
+    "sala1": ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"],
+    "sala2": ["João", "Antonio", "Carlos", "Maria", "Isolda"],
+}
+nome_aulas = {
+    "aula_ingles": "Ingles",
+    "aula_musica": "Musica",
+    "aula_danca": "Dança",
+}
 
-atividades = [
-    ("Inglês", aula_ingles), 
-    ("Música", aula_musica), 
-    ("Dança", aula_danca),
-]
-
-# Listar alunos em cada atividade por sala
-
-for nome_atividade, atividade in atividades:
-
-    print(f"Alunos da atividade {nome_atividade}\n")
+nome_salas = {
+    "sala1": "Sala 01",
+    "sala2": "Sala 02",
+}
+for aula in aulas:
+    print(f"Alunos de {nome_aulas[aula]}\n")
     print("-" * 50)
+    alunos_sala1 = []
+    alunos_sala2 = []
+
+
+    for aluno in aulas[aula]:
+        if aluno in salas["sala1"]:
+            alunos_sala1.append(aluno)
+        elif aluno in salas["sala2"]:
+            alunos_sala2.append(aluno)
+
+
+    print("sala1", alunos_sala1)
+    print("sala2", alunos_sala2)
+
+    #print(salas, {nome_salas[salas]}\n")
+   # print(aulas)
+    print("-" * 50)
+    
 
     # sala1 que tem interseção com a atividade
-    atividade_sala1 = set(sala1) & set(atividade) & set(atividade)
-    atividade_sala2 = set(sala2) & set(atividade).intersection(atividade)
 
-    print("Sala1", atividade_sala1)
-    print("Sala2", atividade_sala2)
+
     
-    print()
-    print("-" * 50)
+    
 
