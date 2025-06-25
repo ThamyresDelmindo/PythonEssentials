f2e288eb657adbf65c67c53c81417139304f1496
Thamyres Delmindo <thamyres.delmindo22@gmail.com>
2025-06-25 15:49:32 -0300

    Finalizando os TODO com o profe e sanitizando os logs com base em bytes

--- logs.py
+++ logs.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env "source .venv/bin/activate" python3
 import os
-import logging 
-
-log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
+import logging
+from logging import handlers 
 # BOILERPLATE - Código repetitivo
 #TODO: usar função pra configurar o log e não precisar ficar repetindo
 #TODO: Depois disso acima, usar uma lib, biblioteca pra facilitar tudo
 # criar nossa propria instancia de logger
+
+log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 log = logging.Logger("logs.py", log_level)  #é só a definição do nome que vai aparecer quando der o erro, posso usar qualquer nome na string
-# level
-ch = logging.StreamHandler() #console/terminal/stderr
-ch.setLevel(log_level)
-#formatação
+#ch = logging.StreamHandler() #console/terminal/stderr
+#ch.setLevel(log_level)
+fh = handlers.RotatingFileHandler(
+    "meulog.log", 
+    maxBytes=300, #10**6 recomendado
+    backupCount=10,
+)
+fh.setLevel(log.level)
 fmt = logging.Formatter(
     '%(asctime)s %(name)s %(levelname)s ' 
     'l:%(lineno)d f:%(filename)s: %(message)s'
 )
-ch.setFormatter(fmt)
+fh.setFormatter(fmt)
 #destino
-log.addHandler(ch)
+log.addHandler(fh)
 
 
 """
@@ -30,7 +35,7 @@ log.error("Erro que afeta uma única execução")
 log.critical("Erro geral. Ex. O banco de dados sumiu")
 """
 
-print("---")
+#print("---")
 
 try:
     1 / 0
