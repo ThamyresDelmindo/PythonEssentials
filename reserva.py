5226e32d1b2537b1e2404393c02dc4f0b86cacc4
Thamyres Delmindo <thamyres.delmindo22@gmail.com>
2025-07-12 02:31:22 -0300

    Ultimo projeto do PythonEssentials finalizado

--- reserva.py
+++ reserva.py
@@ -1,27 +1,51 @@
-"""
-Faça um programa de terminal que exibe ao usuário uma lista dos quartos disponíveis
-para alugar e o preço de cada quarto, esta informação está disponível em um arquivo
-de texto separado por vírgulas.
+import sys
 
-'quartos.txt'
+em_uso = open("reservas.txt").readlines()
 
-#codigo, nome, preço
-1, Suite Master, 500
-2, Quarto Família, 200
-3, Quarto Single, 100
-4, Quarto Simples, 50
+print("Segue nosso catálogo de quartos disponíveis para reserva")
 
-O programa pergunta ao usuário o nome, qual o número do quarto a ser reservado e a 
-quantidade de dias e no final exibe o valor estimado a ser pago.
+try:
+    quartos = open("quartos.txt").readlines()
+except FileNotFoundError as e:
+    print(f"{str(e)}.")
+    sys.exit(1)
+print(quartos)
 
-O programa deve salvar esta escolha em outro arquivo contendo as reservas
+valores = {
+    1: 500,
+    2: 200,
+    3: 100,
+    4: 50
+}
 
-'reservas.txt'
+catalogo = {
+    1: "Suite Master",
+    2: "Quarto Familia",
+    3: "Quarto Single",
+    4: "Quarto Simples"
+}
 
-#cliente, quarto, dias
-Bruno, 3, 12
-....
+nome_cliente = input("Digite seu nome")
+quarto_desejado = int(input("Qual o quarto desejado?"))
+if quarto_desejado not in valores:
+    print("Escolha inválida, tente novamente escolhendo um quarto por vez com 1, 2, 3 ou 4")
+    sys.exit(1)
 
-Se outro usuário tentar reservar o mesmo quarto o programa deve exibir uma
-mensagem informando que já está reservado.
-"""
+for itens in em_uso[1:]:
+    if quarto_desejado == int(itens.split(",")[1].strip()):
+        print("Este quarto já está em uso")
+        sys.exit(1)
+    else:
+        continue
+dias_reserva = int(input("A reserva é para quantos dias?"))
+
+with open("reservas.txt", "a") as file:
+    novo_cliente = f"{nome_cliente}, {quarto_desejado}, {dias_reserva}\n"
+    file.write(novo_cliente)
+
+
+quarto_escolhido = catalogo[quarto_desejado]
+preco_final = valores[quarto_desejado] * dias_reserva
+
+resumo_do_pedido = f"Sua reserva do quarto {quarto_escolhido} para {dias_reserva} dia(s) ficou no valor de R${preco_final}"
+print(resumo_do_pedido)
